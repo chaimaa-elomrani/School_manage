3d351143699082552300891d7649a75edf411912
chaimaa-elomrani <chaimaelomrani6@gmail.com>
2025-07-16 14:49:02 +0100

    added the student controller methods using the logic of the student service

--- backend/app/controllers/StudentController.php
+++ backend/app/controllers/StudentController.php
@@ -7,15 +7,44 @@ require_once __DIR__ . './models/Student.php';
 //  pour la modification on veut dire par l'extension que on peut ajouter de nouvelles fonctionnalités sans modifier le code existant 
 // Factory method: c'est une méthode qui permet de créer des objets sans avoir à spécifier la classe exacte à instancier
 
-// class StudentController{
+class StudentController
+{
 
-//     private $student;
-//     public function __construct(){
-//         $this->student = new Student();
-//     }
+    private $studentService ; 
 
-// //     public function index(){
-// //         $sudent = PersonFactory::create('student', $request); 
+    public function __construct(StudentService $studentService){ // the params are the dependencies of the class , dependencies are the classes that are used in the class
+        $this->studentService = $studentService; 
+    }
 
-// // }
-//     }
+
+    public function create(array $data){
+        $student = PersonFactory::createPerson($data['role'], $data); 
+        $this->studentService->save($student);
+        return $student ; 
+    }
+
+
+
+    public function getAll(){
+        $students = $this->studentService->getAll();
+        return $students;
+    }
+
+    public function getById($id){
+        $student = $this->studentService->getById($id);
+        return $student;
+    }
+
+    public function update(array $data){
+        $student = PersonFactory::createPerson($data['role'], $data); // ($data['role'], $data) this means that we are passing the role and the data to the createPerson method
+        $this->studentService->update($student);
+        return $student;
+    }
+
+    public function delete($id){
+        $this->studentService->delete($id);
+        return true;
+    }
+
+
+}
